f998017f195549eb9b4945add486c44e18d1ef33
khon@toun28.com <4djrdmfqjfRjdi!>
2021-04-14 18:38:46 +0900

    "code refactoring"

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,24 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
+pub struct Guess {
+    value: u32,
+}
+
+impl Guess {
+    pub fn new(value: u32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}", value);
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> u32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1..101);
@@ -22,6 +40,14 @@ fn main() {
             Ok(num) => num,
             Err(_) => continue,
         };
+
+        //guess = Guess::new(guess).value()
+        println!("{}", Guess::new(guess).value());
+
+        //        if guess < 1 || guess > 100 {
+        //            println!("The secret number will be between 1 and 100.");
+        //            continue;
+        //        }
 
         println!("You guessed: {}", guess);
 
